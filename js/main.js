2977aaf04aa0a6e3f7701539c2e16ad2add8b1c7
Julio <julesnoir@hotmail.fr>
2021-09-16 18:53:18 +0200

    Added player's Shi Fu Mi choice

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-alert("Welcome to Jules's Shi Fu Mi game !");
 
+
+// askName function does as its name implies and restrict user
+// to a name between 2 and 20 chars. value is returned to a playerName variable.
 function askName(){
-    let name = prompt("test");
+    let name = prompt("What is your name ? ( must be between 2 and 20 characters. )");
     if (( name.length < 2 || name.length > 20)){
         alert("Name is incorrect. Please enter your name again");
         askName();
@@ -10,5 +12,21 @@ function askName(){
     }
 }
 
+function askShiFuMi(){
+    let entry = prompt("Choose : Rock, Paper, or Scissors ?\nYou must type your selection")
+    let entryArr = ["rock","paper","scissors"];
+    for (let i = 0; i < entryArr.length;i++){
+        if (entry.toLocaleLowerCase() === entryArr[i]){
+            return entry;
+        }
+    }
+    alert("Entry was incorrect.\nPlease try again.")
+    askShiFuMi();
+}
+
+// Game Loop's logic goes here
+
+alert("Welcome to Jules's Shi Fu Mi game !");
 const playerName = askName();
-alert(`Welcome ${playerName} !`);
+alert(`Welcome ${playerName} !\nYou will play Shi Fu Mi against my super intelligent AI today !`);
+const playerShiFuMi = askShiFuMi();
