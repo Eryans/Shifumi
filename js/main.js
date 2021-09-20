d7cf86c72515e6bd39047cbe5663463eee7b7f09
Julio <julesnoir@hotmail.fr>
2021-09-20 10:51:41 +0200

    Correction commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// !! Code rewrote during exercice correction !!
 let playerName = "";
 let playerInput;
 let playerScore = 0;
@@ -13,16 +14,13 @@ const roundToWin = 3;
 const entryArr = ["rock","paper","scissors"];
 const entryCheck = [{
     entry : entryArr[0],
-    win : entryArr[2],
-    lose : entryArr[1]
+    win : entryArr[2]
 },{
     entry : entryArr[1],
-    win : entryArr[0],
-    lose : entryArr[2]
+    win : entryArr[0]
 },{
     entry : entryArr[2],
-    win : entryArr[1],
-    lose : entryArr[0]
+    win : entryArr[1]
 }];
 
 const winMessage = "\n\nYou win this round !";
@@ -33,13 +31,7 @@ const plAgMessage = "\n\nDo you wish tou play again ?";
 const bigWinMessage = "You win the game ! Congratulation !\n\nDo you want to play again ?";
 const bigLoseMessage = "AI win the game ! Too bad !\n\nDo you want to play again ?";
 
-// Initialization
-alert("Welcome to Jules's Shi Fu Mi game !");
-playerName = askName();
-alert(`Welcome ${playerName} !\nYou will play Shi Fu Mi against my super intelligent AI today !\nYou must win 3 round to score !`);
-// Loop
-gameLoop();  
-
+/* -----------------------------Function------------------------------------------ */
 
 // Game Loop's logic goes here
 function gameLoop(){    
@@ -48,19 +40,16 @@ function gameLoop(){
     aiInput = aiShiFuMi();
     ShiFuMi(playerInput,aiInput);
 }
-
 // Ask name and restrict user to a name between 2 and 20 chars. 
-// Value is returned to a playerName variable.
 function askName(){
     let name = prompt("What is your name ? ( must be between 2 and 20 characters. )");
     if ( name.length < 2 || name.length > 20 || name === undefined){
-        alert("Name is incorrect. Please enter your name again");
+        alert("Name is incorrect. Please enter your name again.");
         askName();
     } else {
         return name;
     }
 }
-
 // Ask for player's input 
 function askShiFuMi(){
     let entry = prompt("Choose : Rock, Paper, or Scissors ?\nYou must type your selection")
@@ -73,20 +62,23 @@ function askShiFuMi(){
     alert("Entry was incorrect.\nPlease try again.")
     askShiFuMi();
 }
-
 // Returns rock,paper,scissors randomly
 function aiShiFuMi(){
-    let random = Math.floor(Math.random() * 3); //Using Math.floor instead of round to avoid returning a 3 which crashes the game
+    let random = Math.floor(Math.random() * entryArr.length); //Using Math.floor instead of round to avoid returning a 3 which crashes the game
     return entryArr[random];
 }
-
+function reset(){
+    playerScore = 0;
+    aiScore = 0;
+    round = 0;
+}
+// Reset score and round and update turn if player wants to replay. 
+// Leave window tab if player wants to leave
 function checkForWinner(){
     if (playerScore >= roundToWin){
         let checkBox = confirm(bigWinMessage); 
-            if (checkBox){ // Reset score and round and update turn if player wants to replay. Leave window tab if player wants to leave
-                playerScore = 0;
-                aiScore = 0;
-                round = 0;
+            if (checkBox){ 
+                reset();
                 playerBigScore++;
                 gameLoop();
             } else {
@@ -96,9 +88,7 @@ function checkForWinner(){
     } else if (aiScore >= roundToWin){
         let checkBox = confirm(bigLoseMessage);
         if (checkBox){
-            playerScore = 0;
-            aiScore = 0;
-            round = 0;
+            reset();
             aiBigScore++;
             gameLoop();
         } else {
@@ -107,13 +97,11 @@ function checkForWinner(){
         }
     }
 }
-
 // Check player and ai input and update score
 // New, beautiful and glorious compare function
 function ShiFuMi(playerSFM,aiSFM){
-    // MEMO : 0 = rock / 1 = paper / 2 = scissors 
     // For some reason using ${} to put playerScore or aiScore in the message string doesn't update the score.
-    const separation = "                                                                               ";
+    const separation = "                                                               ";
     let choices = "\n\n" + playerName + " : " + playerSFM + " VS " + aiSFM + " : AI\n\n";
     let showRound = "\nRound : ";
     let showScore = "\n\nYour current score : "; // Cannot add Score var here or it's not updated on time
@@ -129,25 +117,40 @@ function ShiFuMi(playerSFM,aiSFM){
             if (aiSFM === entryCheck[i].win){
                 playerScore++;
                 round++
-                alert(playerName + ": " + playerBigScore + separation + aiBigScore + ": AI"+ showRound + round + showScore + playerScore + showAiScore+ aiScore + choices + winMessage)
+                alert(playerName + ": " + playerBigScore + separation + aiBigScore + ": AI"+
+                 showRound + round + showScore + playerScore + showAiScore+ aiScore + choices + winMessage)
                 gameLoop();
             } else {
                 aiScore++;
                 round++;
-                alert(playerName + ": " + playerBigScore + separation + aiBigScore + ": AI"+ showRound + round + showScore + playerScore + showAiScore + aiScore + choices + loseMessage)
+                alert(playerName + ": " + playerBigScore + separation + aiBigScore + ": AI"+
+                 showRound + round + showScore + playerScore + showAiScore + aiScore + choices + loseMessage)
                 gameLoop();
             }
         }
     }
 }
 
+/*----------------------------Logic--------------------------- */
+
+alert("Welcome to Jules's Shi Fu Mi game !");
+playerName = askName();
+alert(`Welcome ${playerName} !\nYou will play Shi Fu Mi against my super intelligent AI today !\nYou must win 3 round to score !`);
+gameLoop();  
+
+
+
+
+
+
+
 // Old stinky and ugly ShiFuMi function. I'm letting this here to scare the childrens
 
 /*function ShiFuMi(playerSFM,aiSFM){
     // 0 = rock / 1 = paper / 2 = scissors
-    // if player win or lose a confirm prompt ask him if he wants to play again or leave
-    // For some reason using ${} to put playerScore in the message string doesn't update the score.
-    // TODO find a wind to clean up this spaghetti code. Use loop or recursive func maybe ?
+    // This was written just to have a fonctionnal prototype
+    // if player win or lose a confirm prompt ask him if he wants to play again or leave.
+    // TODO Clean up this spaghetti code. 
     if (playerSFM === aiSFM){
         alert("Current Score : " + playerScore + drawMessage);
         gameLoop(); // if draw go back to main loop
